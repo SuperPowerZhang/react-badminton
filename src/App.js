fce86e8ab9f4d5e6a291fe20e2f3813ee7e8161a
SuperPowerZhang <2285546178@qq.com>
2020-12-13 19:04:51 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  function  get(url) {
+    return new Promise(function(resolve,reject){
+      let req=new XMLHttpRequest()
+      console.log(111111)
+      console.log(url)
+      req.open('GET',url)
+      req.onload=function(){
+        if(req.status===200){
+          resolve(req.response)
+        }else {
+          reject (Error(req.response))
+        }
+      }
+      req.onerror=function () {
+        reject(Error("Network Error"))
+      }
+      req.send()
+    })
+  }
+  get('http://badminton.amberwuwu.com/blog/api/category/').then((response)=>{
+    console.log('成功了，真意外',response)
+  },(error)=>{
+    console.error('oh no',error)
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         hello
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
